14eb19a1dd93072c121a65ab546c761740c44c66
H-silver <khewg3@gmail.com>
2020-05-09 15:01:46 +0900

    nav js

---
+++ js/common.js
@@ -0,0 +1,12 @@
+    function openNav() {
+        document.getElementById("menu").style.width = "45%";
+        document.getElementById("menu").style.paddingLeft = "5%";
+        document.getElementById("menu").style.paddingTop = "5%";
+        document.getElementById("btn-close").style.width = "100px";
+      }
+    function closeNav() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("menu").style.paddingLeft = "0";
+    document.getElementById("menu").style.paddingTop = "0";
+    document.getElementById("btn-close").style.width = "0";
+    }
