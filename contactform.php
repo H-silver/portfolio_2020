72688d643ed97315a0510eaa26ff3a01296caa96
H-silver <61346848+H-silver@users.noreply.github.com>
2020-06-26 06:29:48 +0900

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -6,7 +6,7 @@ if (isset($_POST['submit'])) {
    $mailForm = $_POST['mail'];
    $message = $_POST['message'];
 
-   $mailTo = "khewg3@naver.com";
+   $mailTo = "haeunkang91@naver.com";
    $headers = "From: ".$mailFrom;
    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
